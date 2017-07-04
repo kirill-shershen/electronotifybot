504fa4c98d1f693d4dec4232ab81b9befac56758
kshershen <shkipc@gmail.com>
2017-07-04 13:26:05 +0300

    read config from cloud datastore, fix app.yaml

--- config.py
+++ config.py
@@ -1,22 +1,35 @@
 # -*- coding: utf-8 -*-
 import os
 
+gae = os.environ.get('USER') == 'vol1001_01'
+if gae:
+    from google.appengine.ext import ndb
+    class Settings(ndb.Model):
+        name = ndb.StringProperty()
+        value = ndb.StringProperty()
 
-token = os.environ.get('token')
-db_url = os.environ.get('DATABASE_URL')
+def get(name):
+    if gae:
+        val = Settings.query(Settings.name == name).get()
+        return val.value
+    else:
+        return os.environ.get(name)
+
+token = get('token')
+db_url = get('DATABASE_URL')
 SQLALCHEMY_DATABASE_URI = db_url
-host = os.environ.get('host')
-debug = os.environ.get('debug') == 'True'
-heroku_debug = os.environ.get('heroku_debug') == 'True'
-server_debug = os.environ.get('server_debug')
+host = get('host')
+debug = get('debug') == 'True'
+heroku_debug = get('heroku_debug') == 'True'
+server_debug = get('server_debug')
 try:
-    port_debug = int(os.environ.get('port_debug') or 0)
+    port_debug = int(get('port_debug') or 0)
 except ValueError:
     port_debug = 0
 
-WEBHOOK_HOST = 'electronotifybot.herokuapp.com'
+WEBHOOK_HOST = get('WEBHOOK_HOST')
 WEBHOOK_URL_PATH = '/bot'
-WEBHOOK_PORT = os.environ.get('PORT',5000)
+WEBHOOK_PORT = get('PORT',5000)
 WEBHOOK_LISTEN = '0.0.0.0'
 
 WEBHOOK_URL_BASE = "https://%s/%s"% (WEBHOOK_HOST,WEBHOOK_URL_PATH)
